c5e4cdc4a36e83c19e322a5cc847abd6135d7ebc
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-28 23:53:36 +0200

    Fix rubocop issues

--- problems/5_best-time-to-buy-and-sell-stock.rb
+++ problems/5_best-time-to-buy-and-sell-stock.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit(prices)
-    min = prices.max 
-    max = 0
-    prices.each do |nbr|
-        if nbr < min
-            min = nbr
-        elsif nbr - min > max
-            max = nbr - min
-        end
+  min = prices.max
+  max = 0
+  prices.each do |nbr|
+    if nbr < min
+      min = nbr
+    elsif nbr - min > max
+      max = nbr - min
     end
-    max
+  end
+  max
 end
 
-p max_profit([7,1,5,3,6,4])
+p max_profit([7, 1, 5, 3, 6, 4])
