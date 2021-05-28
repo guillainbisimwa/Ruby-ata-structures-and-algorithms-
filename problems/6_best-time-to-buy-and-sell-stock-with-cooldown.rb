68a7ea9188a2cbf48d4509cbc6023ffdfca175f7
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-29 00:19:53 +0200

    Fix rubocop issues

--- problems/6_best-time-to-buy-and-sell-stock-with-cooldown.rb
+++ problems/6_best-time-to-buy-and-sell-stock-with-cooldown.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit(prices)
-    return 0 if prices.length < 2
+  return 0 if prices.length < 2
 
-    ans_zero = ans_two = 0
-    ans_one = -prices[0]
+  ans_zero = ans_two = 0
+  ans_one = -prices[0]
 
-    (1...prices.length).each do |index|
-        temp = ans_two
-        ans_two = ans_one + prices[index]
-        ans_one = [ans_zero - prices[index], ans_one].max
-        ans_zero = [ans_zero, temp].max
-    end
-    return [ans_zero, ans_two].max
+  (1...prices.length).each do |index|
+    temp = ans_two
+    ans_two = ans_one + prices[index]
+    ans_one = [ans_zero - prices[index], ans_one].max
+    ans_zero = [ans_zero, temp].max
+  end
+  [ans_zero, ans_two].max
 end
 
-p max_profit([1,2,3,0,2])
+p max_profit([1, 2, 3, 0, 2])
