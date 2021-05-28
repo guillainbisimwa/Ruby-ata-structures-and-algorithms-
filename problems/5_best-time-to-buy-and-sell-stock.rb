6701c76ccf2656b9daae39f32b2b3a30d3cac326
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-28 23:52:55 +0200

    Add best-time-to-buy-and-sell-stock solution

---
+++ problems/5_best-time-to-buy-and-sell-stock.rb
@@ -0,0 +1,16 @@
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+    min = prices.max 
+    max = 0
+    prices.each do |nbr|
+        if nbr < min
+            min = nbr
+        elsif nbr - min > max
+            max = nbr - min
+        end
+    end
+    max
+end
+
+p max_profit([7,1,5,3,6,4])
