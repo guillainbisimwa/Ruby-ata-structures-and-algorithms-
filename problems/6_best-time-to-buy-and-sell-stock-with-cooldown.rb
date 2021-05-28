eaa1c6c378205fd03e94630931ab8e219da3d827
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-29 00:19:31 +0200

    Add best-time-to-buy-and-sell-stock-with-cooldown solution

---
+++ problems/6_best-time-to-buy-and-sell-stock-with-cooldown.rb
@@ -0,0 +1,18 @@
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+    return 0 if prices.length < 2
+
+    ans_zero = ans_two = 0
+    ans_one = -prices[0]
+
+    (1...prices.length).each do |index|
+        temp = ans_two
+        ans_two = ans_one + prices[index]
+        ans_one = [ans_zero - prices[index], ans_one].max
+        ans_zero = [ans_zero, temp].max
+    end
+    return [ans_zero, ans_two].max
+end
+
+p max_profit([1,2,3,0,2])
