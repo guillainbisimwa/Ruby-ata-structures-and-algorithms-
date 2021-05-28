eaa044a9028fa1f0f8d034513ff499e1f99be1d0
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-28 23:32:54 +0200

    Add a buggy solultion - Time Limit Exceeded

--- problems/4_3sum.rb
+++ problems/4_3sum.rb
@@ -3,25 +3,27 @@
 require 'set'
 
 def three_sum(nums)
+
+    return [] if nums[0] == 0 && nums.length < 3
+
+    return [[0, 0, 0]] if nums.uniq[0] == 0 && nums.uniq.length == 1
+
     hash_table = []
     index = 0
     nums.sort!
+
     len = nums.length
     (0...len).each do |index|
         complement = Hash.new
+        if (index != 0 && nums[index] == nums[index - 1])
+            next
+        end
         ((index+1)...len).each  do |position|
             t = -nums[index] - nums[position]
-            if (index != 0 && nums[index] == nums[index - 1])
-                next
-            end
             if complement.value?(t)
-                
-                if nums.uniq[0].zero? && nums.uniq.length == 1
-                    hash_table.push([nums[index], nums[position], -nums[index] - nums[position]].sort)
-                    return hash_table
+                unless hash_table.include?([nums[index], nums[position], t].sort)
+                    hash_table.push([nums[index], nums[position], t].sort)
                 end
-                hash_table.push([nums[index], nums[position], -nums[index] - nums[position]].sort)
-
                 while (nums[position+1] == nums[position] && position + 1 < len)
                     position += 1
                 end
@@ -35,4 +37,5 @@ end
 
 # three_sum([-1,0,1,2,-3,-4])
 #p three_sum([0,0,0,0])
-p three_sum([-2,0,0,2,2])
+#p three_sum([-2,0,0,2,2])
+p three_sum([0])
