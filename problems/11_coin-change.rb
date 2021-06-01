603b2cafd9e5526033a7da28dfe5da81658e548f
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-06-01 19:23:24 +0200

    Fix rubocop issues

--- problems/11_coin-change.rb
+++ problems/11_coin-change.rb
@@ -1,21 +1,19 @@
+# frozen_string_literal: true
+
 # @param {Integer[]} coins
 # @param {Integer} amount
 # @return {Integer}
 def coin_change(coins, amount)
-    t = Array.new(amount + 1, amount + 1)  
-    t[0] = 0
-    (1..amount).each do |nbr|
-        temp = coins[nbr]
-        (0...coins.length).each do |j|
-            if coins[j] <= nbr
-                t[nbr] = [t[nbr], t[nbr - coins[j]] + 1].min;
-            end
-            #t[j] += t[j - temp]
-        end 
+  table = Array.new(amount + 1, amount + 1)
+  table[0] = 0
+  (1..amount).each do |nbr|
+    temp = coins[nbr]
+    (0...coins.length).each do |jjj|
+      table[nbr] = [table[nbr], table[nbr - coins[jjj]] + 1].min if coins[jjj] <= nbr
     end
-    t[amount] > amount ? -1 : t[amount];
+  end
+  table[amount] > amount ? -1 : table[amount]
 end
 
-
 p coin_change([1, 3, 5, 7], 8)
-p coin_change([1,2,5], 11)
+p coin_change([1, 2, 5], 11)
