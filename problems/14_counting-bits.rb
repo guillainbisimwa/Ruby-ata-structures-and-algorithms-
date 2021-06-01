4b6156f221ea70f5112bce9aa615387ed3c46b49
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-06-01 23:01:06 +0200

    Fix rubocop issues

--- problems/14_counting-bits.rb
+++ problems/14_counting-bits.rb
@@ -1,15 +1,15 @@
+# frozen_string_literal: true
+
 # @param {Integer} n
 # @return {Integer[]}
 def count_bits(n)
-    result = Array.new(n+1, 0)
-    offset = 1
-    (1...n+1).each do |index|
-        if offset * 2 == index
-           offset *= 2 
-        end
-        result[index] = result[index - offset] + 1
-    end
-    result
+  result = Array.new(n + 1, 0)
+  offset = 1
+  (1...n + 1).each do |index|
+    offset *= 2 if offset * 2 == index
+    result[index] = result[index - offset] + 1
+  end
+  result
 end
 
 p count_bits(5)
