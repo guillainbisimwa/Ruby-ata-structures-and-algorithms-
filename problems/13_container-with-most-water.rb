2fe959eec4a5557fef9efb79ccb12e3b9a1dfff6
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-06-01 22:40:18 +0200

    Fix rubocop issues

--- problems/13_container-with-most-water.rb
+++ problems/13_container-with-most-water.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 # @param {Integer[]} height
 # @return {Integer}
 def max_area(height)
-    max = 0
-    l = 0
-    r = height.length - 1
-    while l < r
-        max = [max, [height[l], height[r]].min * (r-l)].max
-        if height[l] < height[r]
-            l += 1
-        else
-            r -= 1
-        end
+  max = 0
+  left = 0
+  right = height.length - 1
+  while left < right
+    max = [max, [height[left], height[right]].min * (right - left)].max
+    if height[left] < height[right]
+      left += 1
+    else
+      right -= 1
     end
-    max
+  end
+  max
 end
 
-p max_area([1,8,6,2,5,4,8,3,7])
+p max_area([1, 8, 6, 2, 5, 4, 8, 3, 7])
