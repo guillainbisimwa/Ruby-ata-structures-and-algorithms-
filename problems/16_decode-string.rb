d38516dd851afa4f041a37dcf99e21750290f518
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-06-02 19:38:30 +0200

    Fix rubocop issues

--- problems/16_decode-string.rb
+++ problems/16_decode-string.rb
@@ -1,29 +1,31 @@
+# frozen_string_literal: true
+
 # @param {String} s
 # @return {String}
 def decode_string(s)
-    k = 0
-    ans = ''
-    stack = []
-    s.split('').each do |c|
-        if c == '['
-            stack.push(k)
-            stack.push(ans)
-            k = 0
-            ans = ''
-        elsif c == ']'
-            first = stack.pop
-            times = ans * stack.pop
+  k = 0
+  ans = ''
+  stack = []
+  s.split('').each do |c|
+    if c == '['
+      stack.push(k)
+      stack.push(ans)
+      k = 0
+      ans = ''
+    elsif c == ']'
+      first = stack.pop
+      times = ans * stack.pop
 
-            ans = first.concat(times)  
-        elsif c.to_i > 0 || c == "0"
-            k = 10 * k + c.to_i
-        else
-            ans += c
-        end
+      ans = first.concat(times)
+    elsif c.to_i.positive? || c == '0'
+      k = 10 * k + c.to_i
+    else
+      ans += c
     end
-    ans
+  end
+  ans
 end
 
-p decode_string("3[a]2[bc]")
-p decode_string("3[a2[c]]")
-p decode_string("100[leetcode]")
+p decode_string('3[a]2[bc]')
+p decode_string('3[a2[c]]')
+p decode_string('100[leetcode]')
