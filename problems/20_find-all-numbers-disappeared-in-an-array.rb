ea91b9f811ae0d86cf3a6dc147c66ff1f6693b9f
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-06-04 15:05:08 +0200

    Add find_disappeared_numbers solution

---
+++ problems/20_find-all-numbers-disappeared-in-an-array.rb
@@ -0,0 +1,11 @@
+# @param {Integer[]} nums
+# @return {Integer[]}
+def find_disappeared_numbers(nums)
+    array = (1..nums.length).to_a
+    array - nums
+    
+end
+
+p find_disappeared_numbers([4,3,2,7,8,2,3,1])
+
+p find_disappeared_numbers([1,1])
