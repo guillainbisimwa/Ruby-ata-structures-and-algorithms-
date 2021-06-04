450fe1ce0e548cf5562bfedb99978273f3aa259e
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-06-04 15:05:45 +0200

    Fix rubocop issues:

--- problems/20_find-all-numbers-disappeared-in-an-array.rb
+++ problems/20_find-all-numbers-disappeared-in-an-array.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 # @param {Integer[]} nums
 # @return {Integer[]}
 def find_disappeared_numbers(nums)
-    array = (1..nums.length).to_a
-    array - nums
-    
+  array = (1..nums.length).to_a
+  array - nums
 end
 
-p find_disappeared_numbers([4,3,2,7,8,2,3,1])
+p find_disappeared_numbers([4, 3, 2, 7, 8, 2, 3, 1])
 
-p find_disappeared_numbers([1,1])
+p find_disappeared_numbers([1, 1])
