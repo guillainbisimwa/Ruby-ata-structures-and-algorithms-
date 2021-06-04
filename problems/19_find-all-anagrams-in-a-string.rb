60e1cb7ba7b02bb825476efb4710441a2bdf5244
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-06-04 14:44:54 +0200

    Fix rubocop issues

--- problems/19_find-all-anagrams-in-a-string.rb
+++ problems/19_find-all-anagrams-in-a-string.rb
@@ -1,50 +1,44 @@
+# frozen_string_literal: true
+
 # @param {String} s
 # @param {String} p
 # @return {Integer[]}
 def find_anagrams(s, pa)
-    str = s.split('')
-    pattern = pa.split('')
+  str = s.split('')
+  pattern = pa.split('')
 
-    window_start = 0
-    matched = 0
+  window_start = 0
+  matched = 0
 
-    char_frequency = Hash.new
-    anagrams = []
+  char_frequency = {}
+  anagrams = []
 
-    pattern.each do |chr|
-        unless char_frequency.include?(chr)
-            char_frequency[chr] = 0 
-        end
-        char_frequency[chr] += 1
+  pattern.each do |chr|
+    char_frequency[chr] = 0 unless char_frequency.include?(chr)
+    char_frequency[chr] += 1
+  end
+
+  (0...str.length).each do |window_end|
+    right_char = str[window_end]
+    if char_frequency.include?(right_char)
+      char_frequency[right_char] -= 1
+      matched += 1 if (char_frequency[right_char]).zero?
     end
 
-    (0...str.length).each do |window_end|
-        right_char = str[window_end]
-        if char_frequency.include?(right_char)
-            char_frequency[right_char] -= 1
-            if char_frequency[right_char] == 0
-                matched += 1                
-            end
-        end
+    anagrams.append(window_start) if matched == char_frequency.length
 
-        if matched == char_frequency.length
-            anagrams.append(window_start)
-        end
+    next unless window_end >= pattern.length - 1
 
-        if window_end >= pattern.length - 1
-            left_char = str[window_start]
-            window_start += 1
+    left_char = str[window_start]
+    window_start += 1
 
-            if char_frequency.include?(left_char)
-                if char_frequency[left_char] == 0
-                    matched -= 1
-                end
-                char_frequency[left_char] += 1
-            end
-        end
+    if char_frequency.include?(left_char)
+      matched -= 1 if (char_frequency[left_char]).zero?
+      char_frequency[left_char] += 1
     end
-    anagrams
+  end
+  anagrams
 end
 
-p find_anagrams("cbaebabacd", "abc") # [0, 6]
-p find_anagrams("abab", "ab") # [0, 1, 2]
+p find_anagrams('cbaebabacd', 'abc') # [0, 6]
+p find_anagrams('abab', 'ab') # [0, 1, 2]
