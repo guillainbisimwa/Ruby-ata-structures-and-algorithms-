af3477d808860a0140b817b45465489ba0c36328
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-06-07 12:57:11 +0200

    Fix rubocop issues

--- problems/21_find-first-and-last-position-of-element.rb
+++ problems/21_find-first-and-last-position-of-element.rb
@@ -1,27 +1,28 @@
+# frozen_string_literal: true
+
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer[]}
 def search_range(nums, target)
-    start = firstGreaterEqual(nums, target)
-    if start == nums.length || nums[start] != target
-        return [-1, -1]
-    end
-    return [start, firstGreaterEqual(nums, target + 1) - 1 ]
+  start = first_greater_equal(nums, target)
+  return [-1, -1] if start == nums.length || nums[start] != target
+
+  [start, first_greater_equal(nums, target + 1) - 1]
 end
 
-def firstGreaterEqual(nums, target)
-    low = 0
-    high = nums.length
-    while low < high
-        mid = low + ((high - low)/2)
-        if nums[mid] < target
-            low = mid + 1
-        else
-            high = mid
-        end
+def first_greater_equal(nums, target)
+  low = 0
+  high = nums.length
+  while low < high
+    mid = low + ((high - low) / 2)
+    if nums[mid] < target
+      low = mid + 1
+    else
+      high = mid
     end
-    return low
+  end
+  low
 end
 
-p search_range([5,7,7,8,8,10], 8) # [3,4]
-p search_range([5,7,7,8,8,10], 6) # [-1, -1]
+p search_range([5, 7, 7, 8, 8, 10], 8) # [3,4]
+p search_range([5, 7, 7, 8, 8, 10], 6) # [-1, -1]
