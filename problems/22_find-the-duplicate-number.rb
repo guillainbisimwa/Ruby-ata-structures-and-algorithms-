418b56390032f09c92f6f74b489b11caee30c4cf
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-06-07 14:03:09 +0200

    Fix rubocop issues

--- problems/22_find-the-duplicate-number.rb
+++ problems/22_find-the-duplicate-number.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
+
 # @param {Integer[]} nums
 # @return {Integer}
 def find_duplicate(nums)
-    visited = [false] * (nums.length + 1)
-    (0...nums.length).each do |index|
-        if visited[nums[index]]
-            return nums[index]
-        end
-        visited[nums[index]] = true
-    end
-    return -1
+  visited = [false] * (nums.length + 1)
+  (0...nums.length).each do |index|
+    return nums[index] if visited[nums[index]]
+
+    visited[nums[index]] = true
+  end
+  -1
 end
 
 p find_duplicate([1, 2, 3, 4, 4])
 p find_duplicate([2, 2, 2, 2, 2])
-p find_duplicate([1,4,4,2,4])
+p find_duplicate([1, 4, 4, 2, 4])
